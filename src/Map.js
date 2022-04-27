740f2461cd2dc67f7478c804ee9cdaa11a1e656f
sooparkdev <135008690+sooparkdev@users.noreply.github.com>
2022-04-27 10:24:58 -0700

    added media queries

--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,7 @@ import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const Map = () => {
 
+    console.log( process.env.REACT_MAP_GOOGLE_MAPS_API_KEY); /* check */
     const { isLoaded } = useJsApiLoader({ /* this returns whether the map is loaded or not */
         id: 'google-map-script',
         googleMapsApiKey: process.env.REACT_MAP_GOOGLE_MAPS_API_KEY,
