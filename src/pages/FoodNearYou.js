5bd40f80cc9dceb56b59c92e07498e79fb83269c
sooparkdev <135008690+sooparkdev@users.noreply.github.com>
2022-05-29 20:12:45 -0700

    JC Food Near You Prompt

--- src/pages/FoodNearYou.js
+++ src/pages/FoodNearYou.js
@@ -1,3 +1,6 @@
+// Import CSS Styles
+import '../styles/Form.css'
+
 import React, { useState } from "react";
 
 // Import Components
@@ -12,7 +15,10 @@ const FoodNearYou = () => {
     const [hasError, setHasError] = useState(false);
     // console.log(foodResourcesToDisplay); 
     return ( 
-        <div className="food-near-you">
+        <div className="page">
+            <h1>Food Near You</h1>
+            <br></br>
+            <p>Input your children's elementary school to locate the nearest food resources from their school with a preferred distance in miles.</p>
             <FilterBar setfoodResourcesToDisplay={setfoodResourcesToDisplay} setSearchClickedAtLeastOnce={setSearchClickedAtLeastOnce} setMarkerIsClicked={setMarkerIsClicked} setSchoolToDisplay={setSchoolToDisplay} setHasError={setHasError} />
             <Map foodResourcesToDisplay={foodResourcesToDisplay} searchClickedAtLeastOnce={searchClickedAtLeastOnce} setMarkerIsClicked={setMarkerIsClicked} markerIsClicked={markerIsClicked} schoolToDisplay={schoolToDisplay} hasError={hasError} />
         </div>
